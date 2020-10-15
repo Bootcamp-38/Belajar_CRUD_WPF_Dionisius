3a78c3a672dfa4463ab810b07ece186ecaad9dbc
dionisiusyose <dionisiusyose11@gmail.com>
2020-10-15 20:38:48 +0700

    versi_2

--- Belajar_CRUD_WPF_Dionisius/Belajar_CRUD_WPF_Dionisius/MainWindow.xaml.cs
+++ Belajar_CRUD_WPF_Dionisius/Belajar_CRUD_WPF_Dionisius/MainWindow.xaml.cs
@@ -28,33 +28,40 @@ namespace Belajar_CRUD_WPF_Dionisius
         public MainWindow()
         {
             InitializeComponent();
+            // Menampilkan DATA GRID
             TableSupplier.ItemsSource = myContext.Suppliers.ToList();
-            tb_id.IsEnabled = false;
+            // Membuat Id tidak bisa diganti
+            textBoxId.IsEnabled = false;
         }
 
         private void TableSupplier_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(tb_id.Text == string.Empty || tb_name.Text == string.Empty)
+            try
             {
                 object item = TableSupplier.SelectedItem;
 
-                // BERFUNGSI UNTUK MEMANGGIL TABEL KE TEXTBOX
+                // Memanggil Tabel ke TextBox
                 string ID = (TableSupplier.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
-                tb_id.Text = ID;
+                textBoxId.Text = ID;
                 string Name = (TableSupplier.SelectedCells[1].Column.GetCellContent(item) as TextBlock).Text;
-                tb_name.Text = Name;
-            }   
+                textBoxName.Text = Name;
+            }
+            catch (Exception)
+            {
+                
+            }
+                
         }
 
        
 
-        private void btn_input_Click(object sender, RoutedEventArgs e)
+        private void ButtonInputClick(object sender, RoutedEventArgs e)
         {
-            var input = new Supplier(tb_name.Text);
-            if (tb_name.Text == "")
+            var input = new Supplier(textBoxName.Text);
+            if (textBoxName.Text == string.Empty)
             {
                 MessageBox.Show("Tidak ada data yang dimasukkan");
-                tb_name.Focus();
+                textBoxName.Focus();
                 
             }
             else {
@@ -63,36 +70,37 @@ namespace Belajar_CRUD_WPF_Dionisius
 
                 MessageBox.Show("Input Berhasil");
                 TableSupplier.ItemsSource = myContext.Suppliers.ToList();
+                textBoxName.Clear();
             }
             
         }
 
-        private void btn_update_Click(object sender, RoutedEventArgs e)
+        private void ButtonUpdateClick(object sender, RoutedEventArgs e)
         {
-            if(tb_name.Text == string.Empty)
+            if(textBoxName.Text == string.Empty || textBoxId.Text == string.Empty)
             {
-                MessageBox.Show("What is the data!!!!");
+                MessageBox.Show("Pilih Data Terlebih Dahulu");
             }
             else
             {
                 int Id = (TableSupplier.SelectedItem as Supplier).Id;
 
                 Supplier update = myContext.Suppliers.Where(id => id.Id == Id).Single();
-                update.Name = tb_name.Text;
+                update.Name = textBoxName.Text;
                 myContext.SaveChanges();
 
                 MessageBox.Show("Data Berhasil Update");
                 TableSupplier.ItemsSource = myContext.Suppliers.ToList();
-                tb_name.Clear();
-                tb_id.Clear();
+                textBoxName.Clear();
+                textBoxId.Clear();
             }
         }
 
-        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        private void ButtonDeleteClick(object sender, RoutedEventArgs e)
         {
-            if(tb_name.Text == string.Empty)
+            if(textBoxName.Text == string.Empty || textBoxId.Text == string.Empty)
             {
-                MessageBox.Show("Tidak ada data");
+                MessageBox.Show("Tidak ada data yang ingin dihapus");
             }
             else {
                 int Id = (TableSupplier.SelectedItem as Supplier).Id;
@@ -103,8 +111,8 @@ namespace Belajar_CRUD_WPF_Dionisius
 
                 MessageBox.Show("Data Berhasil Dihapus");
                 TableSupplier.ItemsSource = myContext.Suppliers.ToList();
-                tb_name.Clear();
-                tb_id.Clear();
+                textBoxName.Clear();
+                textBoxId.Clear();
             }
         }
     }
